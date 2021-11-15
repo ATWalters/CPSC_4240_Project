e4bd8031bbf6f772537bb605f61fc0a935a5013a
ATWalters <austintwalters1998@gmail.com>
2021-11-15 15:25:42 -0500

    almost done with the adduser part of user management

--- user_manage.cpp
+++ user_manage.cpp
@@ -1,5 +1,106 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+vector<string> getUsers(){
+    vector<string> users;
+    //Command to display all of the users in the system and store in temp.txt
+    system("eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 > temp.txt");
+
+    //Open temp.txt and read line by line the users in the system and store in an array for printing to the screen to give users options to choose from
+    string line;
+    ifstream myfile ("temp.txt");
+    if(myfile.is_open()){
+        while(getline(myfile, line)){
+            users.push_back(line);
+        }
+    }else{
+        cout << "Problem opening file temp.txt! \n Exiting program";
+    }
+
+    return users;
+}
+
+void add_user(){
+    //Get username from the sys admin
+    string userName;
+    cout << "Enter the username for the new user" << endl;
+    cin >> userName;
+
+    //Determine if sys admin wants to create a home directory for this user
+    char createHome;
+    cout << "Do you want to create a home directory for the user? [Y/N]" << endl;
+    cin >> createHome;
+
+    //Determine if sys admin wants to give a specific UID for the new user or not
+    char specificUID;
+    cout << "Do you want to use a specific UID for the new user? [Y/N]" << endl;
+    cin >> specificUID;
+
+    if(specificUID == 'y' || specificUID == 'Y'){
+        string UID;
+        cout << "Enter the UID for the user" << endl;
+        cin >> UID;
+    }
+
+    //Determine if sys admin wants to add new user to a specific group or not
+    char specificGroup;
+    cout << "Do you want to add the new user to an existing group? [Y/N]" << endl;
+    cin >> specificGroup;
+
+    if(specificGroup == 'y' || specificGroup == 'Y'){
+        string group;
+        cout << "Enter the group name to add the user to" << endl;
+        cin >> group;
+    }
+
+    //Build the command to pass to the system
+    string addUserCmd = "useradd ";
+}
+
+void del_user(){
+
+}
+
+void mod_user(){
+    //Vector to store all of the users currently in the system
+    vector<string> users;
+    users = getUsers();
+}
 
 void user_manage(){
+    char option;
+    cout << "Enter [A/a] to add a new user" << endl;
+    cout << "Enter [D/d] to delete a user" << endl;
+    cout << "Enter [M/m] to modify a user" << endl;
+    cout << "Enter [C/c] to cancel and go back" << endl;
+
+    cout << "Enter an option:" << endl;
+    cin >> option;
     
+    switch(option){
+        //Call add user function
+        case 'A':
+        case 'a':
+            add_user();
+            break;
+        //Call delete user function
+        case 'D':
+        case 'd':
+            del_user();
+            break;
+        //Call modify user function
+        case 'M':
+        case 'm':
+            mod_user();
+            break;
+        case 'C':
+        case 'c':
+            return;
+        default:
+            return;
+    }      
 }
